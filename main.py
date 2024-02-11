7200c02080459bc9b406f9960b2808112ddfa811
NotHiaki <celsohenrique367@gmail.com>
2024-02-11 16:09:23 -0300

    refactor: use recursive function

--- main.py
+++ main.py
@@ -1,36 +1,55 @@
-def getSelectedRowAndShuffleCards(row, cards):
-    match int(row):
-        case '1':
+def startRound(rounds, cards):
+    if rounds > 2:
+        return [card for sublist in cards for card in sublist]
+
+    print('choose one of this cards:\n')
+    print('first row', cards[0])
+    print('second row', cards[1])
+    print('third row', cards[2], '\n')
+
+    row = int(input('which row is your letter on? 1, 2 ou 3?'))
+    rowIsInvalid = row not in [1, 2, 3]
+
+    if rowIsInvalid:
+        print('restart the program and select a valid row.')
+        exit()
+
+    match row:
+        case 1:
             cards[0], cards[1] = cards[1], cards[0]
-        case '2':
-            return
-        case '3':
+        case 2:
+            pass
+        case 3:
             cards[1], cards[2] = cards[2], cards[1]
 
-    tempCards = [card for sublist in cards for card in sublist]
-    arr = [[], [], []]
+    #transform [[], [], []] in []
+    cardsList = [card for sublist in cards for card in sublist]
+    cards = [[], [], []]
 
-    for i in range(len(tempCards)):
+    for i in range(len(cardsList)):
         count = i%3
-        arr[count].append(tempCards[i])
+        cards[count].append(cardsList[i])
 
-    return arr
-
+    return startRound(rounds + 1, cards)
 
 cards = [
-            ['Ac', 'Jp', '9o', '2c', '3s', '4o', '5c'],
-            ['6d', '7s', '8s', '9c', 'Ts', 'Jp', 'Qs'],
-            ['Ad', '2p', '3d', '4d', '5p', '6d', '7d']
-        ]
+    ['A♥', 'J♠', '9♣', '6♣', '3♠', 'Q♣', '5♣'],
+    ['6♦', '7♠', '8♠', '9♠', 'Q♠', 'J♦', 'K♠'],
+    ['A♦', '2♥', '3♦', 'A♠', '5♥', '4♥', '7♦']
+]
 
-print('choose one of this cards:\n')
-print('first row', cards[0])
-print('second row', cards[1])
-print('third row', cards[2], '\n')
+print('''
+┌──────────────────────────────────────────────────────────────┐
+│   ██████╗  ██╗     ██████╗ █████╗ ██████╗ ██████╗ ███████╗   │
+│    ╚════██╗███║    ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔════╝  │
+│    █████╔╝╚██║    ██║     ███████║██████╔╝██║  ██║███████╗   │
+│    ██╔═══╝  ██║    ██║     ██╔══██║██╔══██╗██║  ██║╚════██║  │
+│    ███████╗ ██║    ╚██████╗██║  ██║██║  ██║██████╔╝███████║  │
+│    ╚══════╝ ╚═╝     ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚══════╝  │
+│    Created by @NotHiaki in Github                            │
+└──────────────────────────────────────────────────────────────┘                 
+''')
 
-row = input('which line is your letter on? 1, 2 ou 3?')
-
-if row not in [1, 2, 3]:
-    print('restart the program and select a valid row.')
-
-print(getSelectedRowAndShuffleCards(row, cards))
+rounds = 0
+x = startRound(rounds, cards)
+print('your card is', x[10])
